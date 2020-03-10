e1e8f5bb66654eae84057a1e0401c026a924b9e6
Camilo Cifuentes <camilo.a.cifuentes.-ND@disney.com>
2020-03-09 23:24:23 -0500

    Initial Commit

---
+++ src/main/java/com/ccifuentes/beerservice/web/model/BeerDto.java
@@ -0,0 +1,26 @@
+package com.ccifuentes.beerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private String name;
+    private BeerStyle beerStyle;
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantity;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastUpdatedDate;
+
+}
---
+++ src/main/java/com/ccifuentes/beerservice/web/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package com.ccifuentes.beerservice.web.model;
+
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
