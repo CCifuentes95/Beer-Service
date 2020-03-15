7f03b9e0811d975b047c07dc4b367b09cf7826ce
Camilo Andrés Cifuentes Grass <camilocifuentes95@gmail.com>
2020-03-15 12:27:52 -0500

    Fix type in generator

--- src/main/java/com/ccifuentes/beerservice/commons/domain/BaseEntity.java
+++ src/main/java/com/ccifuentes/beerservice/commons/domain/BaseEntity.java
@@ -18,7 +18,7 @@ public abstract class BaseEntity implements java.io.Serializable {
 
     @Id
     @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenetator")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Type(type="org.hibernate.type.UUIDCharType")
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
